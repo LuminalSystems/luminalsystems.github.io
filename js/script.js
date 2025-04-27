d8aa0a2b70db2d5d8c0eeecb460cfe6dab04c002
Luminal Systems <luminalsystems.dev@gmail.com>
2025-04-26 22:02:21 -0400

    First Initial Upload of LS website.
    This is the first initial push of a basic template intended to serve as the base template for the 
    website "The Luminous Library", hosted on Github Pages for now until such a time where I can host this 
    elsewhere. 
    
    This push contains : 
    -> index.html -- The initial template to serve as the backbone of this website. 
    -> /css/style.css -- Style sheet with black background and soft neon hues. 
    -> /js/script.js -- Serves as initial script to start soft particles and fractals upon page load. 
    -> /js/particles.js -- This is a Script created by Vincent Garreau under the MIT license. I am using it to enhance the website's looks. 
    
    TO DO: 
    -> Add the other files. 
    -> Upload the rest of folders. 
    -> Finish creating the rest of the backbone structure. 
    -> Push first poc code, white paper, project, and blog entry. 
    -> Create professional portfolio.

---
+++ js/script.js
@@ -0,0 +1,78 @@
+/* script.js - ParticleJS config for LuminalSystems.dev aesthetic */
+
+particlesJS('particles-js', {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": ["#9B5DE5", "#00BBF9", "#00F5D4"]
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 0.5,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      }
+    }
+  },
+  "retina_detect": true
+});
